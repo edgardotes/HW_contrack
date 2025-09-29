fb52ee49eae43ef135ef1473c497eb3d394098d2
Dolores Tesillos <edolores@giub.local@climcal2.giub.unibe.ch>
2025-09-29 11:45:18 +0200

    Tradional heatwaves

---
+++ extract_tmax_years.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#set -euo pipefail
+
+#year=1963
+savepath=/scratch2/edolores/era5/TMAX_era5
+mkdir -p ${savepath};
+
+###create grid
+##0.25
+#cdo griddes /mnt/climstor/ecmwf/era5/raw/SFC/data/1959/an_sfc_ERA5_1959-08-26.nc > gridfile.txt
+###0.50
+cdo griddes /mnt/climstor/ecmwf/era5/raw/PL/data/2001/an_pl_ERA5_2001-08-28.nc > gridfile.txt
+
+for year in $(seq 1959 1999); do
+  echo "=== Processing $year ==="
+
+##==extract t2m and compute mean day
+  loadpath=/mnt/climstor/ecmwf/era5/raw/SFC/data/${year}/an_sfc_ERA5_${year}*
+  out="${savepath}/t2m_day_${year}_0p5deg.nc"
+
+  # skip if final yearly file exists
+  if [ -f "$out" ]; then
+    echo "Already have $out, skipping"
+    continue
+  fi
+
+###
+  for fn in $loadpath; do
+#    fn_new=$savepath/$(basename $fn)
+#    output=${fn_new}
+    base="$(basename "${fn%.nc}")"
+    output="${savepath}/${base}_t2m_day_0p5deg.nc"   # distinct processed filename
+    if [ ! -f "$output" ]; then
+        echo $output
+        cdo -P 8 -L -b F32 -sellonlatbox,-180,180,0,90  -remapbil,gridfile.txt -daymax -select,name=t2m $fn $output
+   fi
+  done
+
+#  cdo -mergetime $savepath/an_sfc_ERA5_${year}*.nc $out
+
+# merge only the processed files
+  out="${savepath}/tmax_day_${year}_0p5deg.nc"
+  cdo -mergetime "${savepath}"/an_sfc_ERA5_${year}*.nc "$out"
+#
+
+# optional: 
+  rm -r $savepath/an_sfc_ERA5_${year}*.nc
+
+done
